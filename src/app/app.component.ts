1a1b6979a672ec553ac622d566e5ad6f53811701
NemesiisV <franciscaf.diazg@gmail.com>
2021-09-08 00:37:03 -0300

    add callback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AcidLabsTest-WebApp';
+
+  constructor(private _oidcSecurityService: OidcSecurityService) {
+
+    this._oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken, idToken }) => {
+      this.isAuth = isAuthenticated;
+    });
+
+  }
+
+  private isAuth: boolean = false;
+
+  ngOnInit() {
+    if (window.location.href.indexOf("callback") == -1 && !this.isAuth) {
+      this.login();
+    }
+  }
+
+  login() {
+    this._oidcSecurityService.authorize();
+  }
 }
